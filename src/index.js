ef2f26eca0d07c3ee8e8ab902da17d487d7261bb
joangabriel98 <joan.chornet.machi@cieep.com>
2019-04-17 10:55:37 +0200

    Database create with connection or port 3000

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/API', {
+  useNewUrlParser: true,
+})
+
 app.set('port', process.env.PORT || 3000)
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.listen(app.get('port'))
