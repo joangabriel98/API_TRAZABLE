f346f2e9ba4bcffc635aca9ea8efc803282444df
joangabriel98 <joan.chornet.machi@cieep.com>
2019-04-18 10:46:01 +0200

    Create controller for functiones

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -29,42 +29,6 @@ module.exports = {
     res.status(200).json(user)
   },
 
-  indexMessage: async (req, res, next) => {
-    const messages = await Message.find({})
-    res.status(200).json(messages)
-  },
-
-  newMessage: async (req, res, next) => {
-    const { userId } = req.params
-    const userMessage = await User.findById(userId)
-    const MessageNew = new Message(req.body)
-    MessageNew.user = userMessage._id
-    await MessageNew.save()
-    userMessage.listMessage.push(MessageNew)
-    await userMessage.save()
-    res.status(201).json(MessageNew)
-  },
-
-  idMessage: async (req, res, next) => {
-    const { idMessage } = req.params
-    const message = await Message.findById(idMessage)
-    res.status(200).json(message)
-  },
-
-  idMessageCheck: async (req, res, next) => {
-    const { idMessage } = req.params
-    const message = await Message.findById(idMessage)
-    message.check = true
-    await Message.findByIdAndUpdate(idMessage, message, { new: true })
-    res.status(200).json(message)
-  },
-
-  DeleteMessage: async (req, res, next) => {
-    const { idMessage } = req.params
-    const message = await Message.findByIdAndDelete(idMessage)
-    res.status(200).json(message)
-  },
-
   PepinoSearch: async (req, res, next) => {
     const message = await Message.find({ 'message.body': { $text: { $search: 'pepino' } } })
     res.status(200).json(message)
--- src/routes/messages.js
+++ src/routes/messages.js
@@ -6,14 +6,13 @@ const {
   newMessage,
   idMessage,
   DeleteMessage,
-  PepinoSearch,
   idMessageCheck,
-} = require('../controllers/user')
+} = require('../controllers/message')
 
 router.get('/', indexMessage)
 router.get('/:idMessage', idMessage)
 router.get('/check/:idMessage', idMessageCheck)
-router.get('/messagePepino', PepinoSearch)
 router.delete('/:idMessage', DeleteMessage)
 router.post('/:userId', newMessage)
+
 module.exports = router
